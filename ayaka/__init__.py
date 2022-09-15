2d550e5fded43fe24afee32ea03c0f67391cc967
Su <wxlxy316@163.com>
2022-09-15 23:39:11 +0800

    test

--- ayaka/__init__.py
+++ ayaka/__init__.py
@@ -1,5 +1,2 @@
-# 导出
-from .adapter import *
+from .adapter import MessageEvent, GroupMessageEvent, Message, MessageSegment, Bot, on_event, on_connect, on_send, logger
 from .core import Ayaka, AyakaApp, AyakaDevice, AyakaBot, AyakaTrigger, AyakaStorage, AyakaCache, create_path, create_file, beauty_save, ayaka
-# 初始化
-from . import plugins
