76020844b2e32457cc9013ea959b40899392dbf5
Su <wxlxy316@163.com>
2023-01-05 10:00:42 +0800

    优化代码

--- plugins/星际旅行-nb.py
+++ plugins/星际旅行-nb.py
@@ -130,5 +130,4 @@ async def get_gold():
         user_id = box.user_id
     )
     gold.value += config.gold_each_time
-    gold.save()
     await m12.send(f"real +{config.gold_each_time} / {gold.value}")
--- plugins/星际旅行.py
+++ plugins/星际旅行.py
@@ -110,5 +110,4 @@ async def get_gold():
         user_id = box.user_id
     )
     gold.value += config.gold_each_time
-    gold.save()
     await box.send(f"real +{config.gold_each_time} / {gold.value}")
